81b60d96660b64082801f8cb313e3571ef9865ec
Camroku <cinaryilmaz.gnu@gmail.com>
2022-05-15 20:06:07 +0300

    Type conversion functions and more comments

--- qstd.py
+++ qstd.py
@@ -1,24 +1,37 @@
 # QoLang Standart Library
 from qclasses import VarVal, Var
 
-available_functions = ["func_print", "func_println", "func_input"]
+available_functions = [
+  "func_print",
+  "func_println",
+  "func_input",
+  "func_toInt",
+  "func_toBool",
+  "func_toStr"
+]
 
 def func_print(Variables, args:list):
+  """
+  Print something, multiple values are joined with " " as delimiter.
+  """
   toprint = []
   for arg in args:
-    if isinstance(arg, Var):
-      toprint += [Variables.getVar(arg.value).value]
+    if isinstance(arg, VarVal):
+      toprint += [str(arg.value)]
     else:
-      toprint += [arg.value]
+      toprint += [str(arg)]
 
   print(" ".join(toprint), end="")
   return Variables
 
 def func_println(Variables, args:list):
+  """
+  Print something with newline, multiple values are joined with " " as delimiter.
+  """
   toprint = []
   for arg in args:
-    if isinstance(arg, Var):
-      toprint += [Variables.getVar(arg.value).value]
+    if isinstance(arg, VarVal):
+      toprint += [str(arg.value)]
     else:
       toprint += [str(arg)]
 
@@ -26,7 +39,52 @@ def func_println(Variables, args:list):
   return Variables
 
 def func_input(Variables, args:list):
+  """
+  Get input with specified prompt and assign to specified variable.
+  """
   inp = input(args[0])
   var = VarVal(name=args[1], value=inp)
+  Variables.setVar(var)
+  return Variables
+
+def func_toInt(Variables, args:list):
+  """
+  Convert any type of variable to Int.
+  """
+  out = 0
+  if type(args[0]) == int:
+    out = args[0]
+  if type(args[0]) == str:
+    try:
+      out = int(args[0])
+    except:
+      out = 0 if args[0] == "" else 1
+  if type(args[0]) == bool:
+    out = 1 if args[0] else 0
+  var = VarVal(name=args[1], value=out)
+  Variables.setVar(var)
+  return Variables
+
+def func_toBool(Variables, args:list):
+  """
+  Convert any type of variable to Bool.
+  """
+  out = False
+  if type(args[0]) == int:
+    out = (args[0] == 1)
+  if type(args[0]) == str:
+    out = False if args[0] == "" else True
+  if type(args[0]) == bool:
+    out = out
+  var = VarVal(name=args[1], value=out)
+  Variables.setVar(var)
+  return Variables
+
+def func_toStr(Variables, args:list):
+  """
+  Convert any type of variable to Str.
+  """
+  out = str(args[0])
+  var = VarVal(name=args[1], value=out)
   Variables.setVar(var)
   return Variables
