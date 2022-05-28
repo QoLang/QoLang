d4d1bff64ab62f73cc5307181cbfa92eb466a734
Camroku <cinaryilmaz.gnu@gmail.com>
2022-05-29 00:55:35 +0300

    Another fix

--- qint.py
+++ qint.py
@@ -238,10 +238,10 @@ class Interpreter(NodeVisitor):
           added = VarVal(toexport, toinclude[toexport])
         self.Variables.setVar(added)
     elif os.path.isfile(libpath + node.incfile + ".qo"):
-      qo.run([sys.argv[0], libpath + node.incfile + ".qo")])
+      qo.run([sys.argv[0], libpath + node.incfile + ".qo"])
       for variable in qo.Variables.getVar("__export__").value:
         self.Variables.setVar(qo.Variables.getVar(variable))
-    elif os.path.isfile(libpath + node.incfile + ".py")):
+    elif os.path.isfile(libpath + node.incfile + ".py"):
       toinclude = runpy.run_path(libpath + node.incfile + ".py")
       for toexport in toinclude["qolang_export"]:
         if callable(toinclude[toexport]):
