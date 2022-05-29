c64f21e03d35cd57d1ea949e061d94e5394fd2c0
Camroku <cinaryilmaz.gnu@gmail.com>
2022-05-29 17:04:43 +0300

    Add CLI arguments, 0.2.2

--- qo.py
+++ qo.py
@@ -7,6 +7,8 @@ import sys
 import runpy
 import os
 
+VERSION = "0.2.2"
+
 def run(args):
   # Load standard library
   if os.name == "nt":
@@ -15,7 +17,7 @@ def run(args):
     toinclude = runpy.run_path("/usr/lib/qo/std.py")
   else:
     print("This OS is not supported, yet.")
-    exit(1)
+    sys.exit(1)
   for fn in toinclude["qolang_export"]:
     if callable(toinclude[fn]):
       added = PythonFunc(fn, toinclude[fn])
@@ -30,4 +32,19 @@ def run(args):
   interpreter.interpret()
 
 if __name__ == "__main__":
-  run(sys.argv)
+  if len(sys.argv) == 1 or sys.argv[1][0] == '-':
+    if len(sys.argv) == 1 or sys.argv[1] in ["--help", "-h"]:
+      print(f"""QoLang {VERSION}
+usage: qolang [file|argument]
+
+arguments:
+  --help,    -h   show this message and exit
+  --version, -v   show version and exit""")
+    elif sys.argv[1] in ["--version", "-v"]:
+      print(VERSION)
+    else:
+      print(f"Unknown argument '{sys.argv[1]}'")
+      sys.exit(1)
+    sys.exit(0)
+  else:
+    run(sys.argv)
