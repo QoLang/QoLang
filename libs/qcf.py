08fef90a0f8c4d3757f7764ff4d2d54181f358e9
Camroku <cinaryilmaz.gnu@gmail.com>
2022-06-12 18:36:49 +0300

    A better QCF implementation

--- libs/qcf.py
+++ libs/qcf.py
@@ -6,24 +6,31 @@ qolang_export = {
     "newqcf": "new"
 }
 
+class QCFInstance:
+    def __init__(self, name, file, Variables):
+        self.name = name
+        self.file = file
+        self.Variables = Variables
+    
+    def __str__(self):
+        return f"QCFI({self.name})"
+
 class QCFGet:
-    def __init__(self, QCFI, QCFF):
+    def __init__(self, QCFI):
         self.QCFI = QCFI
-        self.QCFF = QCFF
     
     def __call__(self, Variables, args):
-        return (Variables, Variables.getVar(self.QCFI).value.getVar(args[0]).value)
+        return (Variables, Variables.getVar(self.QCFI).Variables.getVar(args[0]).value)
     
     __code__ = __call__.__code__
 
 class QCFSet:
-    def __init__(self, QCFI, QCFF):
+    def __init__(self, QCFI):
         self.QCFI = QCFI
-        self.QCFF = QCFF
     
     def __call__(self, Variables, args):
         qcfi = Variables.getVar(self.QCFI)
-        qcfi.value.setVar(VarVal(args[0], args[1]))
+        qcfi.Variables.setVar(VarVal(args[0], args[1]))
         Variables.setVar(qcfi)
         return (Variables, None)
     
@@ -31,12 +38,12 @@ class QCFSet:
 
 def newqcf(Variables, args):
     """
-    Export all variables.
+    Create a new QCF instance.
     """
     import qo
     ftr = os.path.join(os.path.dirname(os.path.realpath(sys.argv[1])), args[0])
     qo.run([sys.argv[0], ftr], qcf=True)
-    Variables.setVar(VarVal(args[1], qo.Variables))
-    Variables.setAttr(args[1], 'get', QCFGet(args[1], args[0]))
-    Variables.setAttr(args[1], 'set', QCFSet(args[1], args[0]))
+    Variables.setVar(QCFInstance(args[1], args[0], qo.Variables))
+    Variables.setAttr(args[1], 'get', QCFGet(args[1]))
+    Variables.setAttr(args[1], 'set', QCFSet(args[1]))
     return (Variables, None)
