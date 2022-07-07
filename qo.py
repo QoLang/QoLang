daf8a00759d97ed034fa2af9c70d0699e07b2c07
Camroku <cinaryilmaz.gnu@gmail.com>
2022-07-07 20:24:25 +0300

    Increase version

--- qo.py
+++ qo.py
@@ -7,7 +7,7 @@ import qparser
 from qclasses import PythonFunc, VarVal, Vars, Token, Tokens
 Variables = Vars()
 
-VERSION = "0.7.1-wip5"
+VERSION = "0.7.1-wip6"
 
 
 def run(args, main=False, qcf=False):
